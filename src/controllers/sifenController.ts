c2698d8e190ba338517907aa25d5a30b9cb5bc83
Victor Martinetti <victor.martinetti@digipar.com>
2025-01-18 13:21:40 -0300

    Sending zip lote to sifen

---
+++ src/controllers/sifenController.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
+import { NODE_ENV } from '../config';
+
+
+export async function sentZipToSIFEN(zipId: number, emisorRuc: string,base64zip: string) {
+  const url = NODE_ENV === "production" ? "https://sifen.set.gov.py/de/ws/async/recibe-lote.wsdl" : "https://sifen-test.set.gov.py/de/ws/async/recibe-lote.wsdl";
+  const p12Path = path.resolve(__dirname, `../../certificates/${emisorRuc}.p12`);
+  const p12Password = 'Fp3!yE4y';
+
+  const httpsAgent = new https.Agent({
+    pfx: fs.readFileSync(p12Path),
+    passphrase: p12Password,
+  });
+
+  const soapEnvelope = `
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsd="http://ekuatia.set.gov.py/sifen/xsd">
+      <soap:Header/>
+      <soap:Body>
+        <xsd:rEnvioLote>
+          <xsd:dId>${zipId}</xsd:dId>
+          <xsd:xDE>${base64zip}</xsd:xDE>
+        </xsd:rEnvioLote>
+      </soap:Body>
+    </soap:Envelope>
+  `;
+
+  try {
+    const response = await axios.post(url, soapEnvelope, {
+      headers: {
+        'Content-Type': 'application/soap+xml; charset=utf-8',
+      },
+      httpsAgent,
+    });
+
+    console.log('Response:', response.data);
+    return { success: true, data: response.data };
+  } catch (error) {
+    console.error('Error sending request:', error);
+    return { success: false, error };
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { dDesTiDEList, dDesTipEmiList } from "./constants";
 import { ciudadesList, departamentosList, distritosList } from "./geographic";
 import { getCDCSinDv, validateJSON, eliminarValoresNulos, calcularDV, getXMLFromDocumento, getFullXML, signXML } from "./controllers/documentController";
 import { Invoice } from "./models/invoice";
-import clipboard from 'clipboardy';
+import clipboard from "clipboardy";
+import fs from "fs";
+import archiver from "archiver";
+import { sentZipToSIFEN } from "./controllers/sifenController";
 
 // test database connection with sequelize
 sequelize
@@ -51,7 +54,7 @@ async function processInvoice() {
 
   const dv = calcularDV(cdcSinDv);
   let cdc = `${cdcSinDv}${dv}`;
-  
+
   const documentoConNulos = {
     DE: {
       $: {
@@ -110,7 +113,7 @@ async function processInvoice() {
       },
       gTotSub: getgTotSub(invoiceJSON),
       gCamDEAsoc: getgCamDEAsoc(invoiceJSON),
-    }
+    },
   };
 
   const documento = eliminarValoresNulos(documentoConNulos);
@@ -128,17 +131,17 @@ async function processInvoice() {
   const { dRucEm, IdcSC, CSC } = invoiceJSON;
   const xmlSigned = await signXML(xmlBuffer, dRucEm, cdc, IdcSC, CSC);
   // save xmlSigned and CDC to database
-  try{
+  try {
     await invoice.update({
       xml: xmlSigned,
-      CDC: cdc
+      CDC: cdc,
     });
-    console.log('Firmado!');
-  } catch{
-    console.log('Error saving xmlSigned and CDC to database');
+    console.log("Firmado!");
+  } catch {
+    console.log("Error saving xmlSigned and CDC to database");
   }
   clipboard.writeSync(xmlSigned);
-  return ;
+  return;
 }
 
 async function createInvoicesZip() {
@@ -147,17 +150,18 @@ async function createInvoicesZip() {
     console.log("No pending zip invoices found");
     return;
   }
-  console.log('First invoice customer', invoice.companyId)
+  console.log("First invoice customer", invoice.companyId);
   const invoices = await getAllNoZippedInvoicesByCompany(invoice.companyId);
   if (!invoices.length) {
     console.log("No pending zip invoices found");
     return;
   }
-  console.log('Invoices to zip', invoices.length)
+  console.log("Invoices to zip", invoices.length);
   // create zip record
   // const zip = await Zip.create({
   // fake create zip record
-  const zipId = Number(String(new Date().getTime()).slice(0, 9));
+  const zipId = 173707000;
+  // const zipId = Number(String(new Date().getTime()).slice(0, 9));
   // update invoices with zipId with a transaction
   const t = await sequelize.transaction();
   try {
@@ -176,12 +180,12 @@ async function sendZipToSIFEN() {
   // get zip invoices
   // const zip = await Zip.findOne({ where: { status: 'PENDIENTE' } });
   // fake get zip
-  const zip = { id: 173706043 };
+  const zip = { id: 173707000, emisorRuc: "80018966" };
   if (!zip) {
     console.log("No pending zip found");
     return;
   }
-  const invoices = await Invoice.findAll({attributes: ['id','xml','CDC'], where: { zipId: zip.id } });
+  const invoices = await Invoice.findAll({ attributes: ["id", "xml", "CDC"], where: { zipId: zip.id } });
 
   if (!invoices.length) {
     console.log("No zip invoices found");
@@ -193,15 +197,27 @@ async function sendZipToSIFEN() {
     return rDE;
   });
 
-  console.log('rDEs', rDEs)
-  // clipboard.writeSync(rDEs.join('\n'));
-  console.log('rDEs joined!')
-  // send zip to TiSIFEN
-  // const response = await sendZipToTiSIFEN(zip);
-  // if (response) {
-  //   await zip.update({ sent: true });
-  //   console.log("Zip sent to TiSIFEN");
-  // }
-  // console.log("Error sending zip to TiSIFEN");
-  // return;
+  // create zip file with rDEs
+  const rLoteDE = `<rLoteDE>${rDEs.join("")}</rLoteDE>`;
+  clipboard.writeSync(rLoteDE);
+  console.log("rDEs joined!");
+  // create an xml file buffer with the rLoteDE content
+  // finally, create a zip file buffer and convert it to base64 string
+  fs.writeFileSync(`${zip.id}.xml`, rLoteDE);
+  // Create zip
+  const output = fs.createWriteStream(`${zip.id}.zip`);
+  const archive = archiver("zip", { zlib: { level: 9 } });
+
+  archive.pipe(output);
+  archive.file(`${zip.id}.xml`, { name: `${zip.id}.xml` });
+  archive.finalize();
+
+  output.on("close", async () => {
+    const zipData = fs.readFileSync(`${zip.id}.zip`);
+    const base64Zip = zipData.toString("base64");
+    console.log(base64Zip);
+    const result = await sentZipToSIFEN(zip.id, zip.emisorRuc, base64Zip);
+    console.log(result);
+    // delete zip file and xml file
+  });
 }
