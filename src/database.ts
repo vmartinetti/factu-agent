da5fc66f151fba2ada8d8928174b3f0e40a44570
Victor Martinetti <victor.martinetti@digipar.com>
2025-01-31 10:19:17 -0300

    connection adjustment

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,19 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "./config";
 
-// TODO: uncomment this line to enable SSL depending on the environment
-// export const sequelize = new Sequelize(DATABASE_URL, { ssl: true, dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } });
-export const sequelize = new Sequelize(DATABASE_URL);
+let sequelize: Sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(DATABASE_URL);
+}
+
+export { sequelize };
