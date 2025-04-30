db0464f9d11fcf5b01eb79266a62b63ecd3e068d
Victor Martinetti <victor.martinetti@digipar.com>
2025-04-30 10:06:45 -0300

    Add condition to filter pending credit and invoice emails with non-null XML

--- src/controllers/invoiceController.ts
+++ src/controllers/invoiceController.ts
@@ -286,6 +286,7 @@ export async function getFirstPendingEmailInvoice() {
       where: {
         annulled: false,
         emailStatus: "PENDIENTE",
+        xml: { [Op.ne]: null },
       },
       logging: false,
     });
